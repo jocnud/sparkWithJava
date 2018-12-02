7ef22e56da81983408360c00a360e42f9f721994
Shahbaz <jocnud@gmail.com>
2018-12-02 13:36:04 +0530

    basic spark with transformation examples

---
+++ src/main/java/com/shahbaz/spark/App.java
@@ -0,0 +1,44 @@
+package com.shahbaz.spark;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.lit;
+
+/**
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        //create a session
+        SparkSession sparkSession = new SparkSession.Builder()
+                .appName("CSV to DB")
+                .master("local")
+                .getOrCreate();
+
+        //load data into dataset
+        Dataset<Row> dataframe = sparkSession.read().format("csv")
+                .option("header", "true")
+                .load("src/main/resources/name_and_comments.txt");
+
+        // adding a new column in dataframe
+        //note that dataframe is immutable so we need to re-assign it back to dataframe after transformation
+        dataframe = dataframe.withColumn("full_name",
+                concat(dataframe.col("last_name"), lit(", "), dataframe.col("first_name")));
+        dataframe.show();
+
+
+        // filtering column which has only numbers in the comments
+        dataframe = dataframe.filter(dataframe.col("comment").rlike("\\d+"));
+        dataframe.show();
+
+        //order by
+        dataframe = dataframe.orderBy(dataframe.col("last_name").asc());
+        dataframe.show();
+
+
+    }
+}
